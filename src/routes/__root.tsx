9fcb32e55ec8d5bc5418cf3acc2e7b922a595c8f
Callum <callum.mcintyre@anza.xyz>
2026-01-03 16:30:44 +0000

    Fix title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,7 @@ export const Route = createRootRoute({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "TanStack Start Starter",
+				title: "Swapsies",
 			},
 			{
 				name: "apple-mobile-web-app-title",
